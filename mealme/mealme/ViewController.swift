f6877591b623d77128d3ce1f45c90733024df329
artem.talko <ipz211_tayu@student.ztu.edu.ua>
2024-01-03 02:18:14 +0200

    init commit

---
+++ mealme/mealme/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mealme
+//
+//  Created by Artem Talko on 03.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
